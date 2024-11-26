63742d581b182a15cb956dbdbb1d9dc79b8f1236
bettyje <bettyjelagat156@gmail.com>
2024-11-26 18:51:18 +0300

    Python's list, tuple, range, and str data type

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < n:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence[:n])
+
+
+print_fibonacci(9)
